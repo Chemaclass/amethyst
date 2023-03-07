5c5ed9cbbfbc88a21faa980662906d601071c551
Chemaclass <chemaclass@outlook.es>
2023-03-07 13:31:17 +0100

    WIP UserZaps.groupZapsByUser()

--- app/src/main/java/com/vitorpamplona/amethyst/service/model/zaps/UserZaps.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/model/zaps/UserZaps.kt
@@ -4,13 +4,54 @@ import com.vitorpamplona.amethyst.model.Note
 import com.vitorpamplona.amethyst.service.model.LnZapEventInterface
 
 object UserZaps {
-  fun forProfileFeed(zaps: Map<Note, Note?>?): List<Pair<Note, Note>> {
+  fun groupZapsByUser(zaps: Map<Note, Note?>?): List<Pair<Note, Note>> {
     if (zaps == null) return emptyList()
+
+//    "Alex": [
+//      Work(workPlace="workPlace", years=1),
+//      Work(workPlace="workPlace", years=4),
+//      Work(workPlace="workPlace", years=5),
+//      Work(workPlace="workPlace", years=1)],
+//    "John": [
+//      Work(workPlace="workPlace", years=2),
+//      Work(workPlace="workPlace", years=2),
+//      Work(workPlace="workPlace", years=1),
+//      Work(workPlace="workPlace", years=6)]
+//
+//    val people: Map<String, List<Work>> = ...
+//    val peopleToSumOfYears: Map<String, Int> =
+//      people.mapValues { (name, works) -> works.sumOf { it.years } }
+
+    val m: Map<String?, List<Pair<Note, Note?>>> = zaps
+      .filter { it.value != null }
+      .toList()
+      .sortedBy { (it.second?.event as? LnZapEventInterface)?.amount() }
+      .groupBy { it.first.author?.pubkeyHex }
+
+    val o = m.mapValues { (pubkey, pairs) ->
+      pairs.fold(0) { acc, pair ->
+        acc = acc + (pair.second?.event as? LnZapEventInterface)?.amount()
+      }
+    }
+
+    var p = o.keys
+
+//    val peopleToSumOfZaps = m.forEach { (pubkey, pairs) ->
+////      (it.second?.event as? LnZapEventInterface)?.amount())
+//      pairs.map
+//    }
 
     return (zaps
       .filter { it.value != null }
       .toList()
       .sortedBy { (it.second?.event as? LnZapEventInterface)?.amount() }
+//      .groupBy { it.first.author?.pubkeyHex }
       .reversed()) as List<Pair<Note, Note>>
+
+//    return (zaps
+//      .filter { it.value != null }
+//      .toList()
+//      .sortedBy { (it.second?.event as? LnZapEventInterface)?.amount() }
+//      .reversed()) as List<Pair<Note, Note>>
   }
 }
--- app/src/test/java/com/vitorpamplona/amethyst/service/zaps/UserZapsTest.kt
+++ app/src/test/java/com/vitorpamplona/amethyst/service/zaps/UserZapsTest.kt
@@ -1,6 +1,5 @@
 package com.vitorpamplona.amethyst.service.zaps
 
-import com.vitorpamplona.amethyst.model.HexKey
 import com.vitorpamplona.amethyst.model.Note
 import com.vitorpamplona.amethyst.service.model.LnZapEventInterface
 import com.vitorpamplona.amethyst.service.model.zaps.UserZaps
@@ -12,41 +11,64 @@ import java.math.BigDecimal
 
 class UserZapsTest {
   @Test
-  fun nothing() {
-    Assert.assertEquals(1, 1)
-  }
-
-  @Test
   fun user_without_zaps() {
-    val actual = UserZaps.forProfileFeed(zaps = null)
+    val actual = UserZaps.groupZapsByUser(zaps = null)
 
-    Assert.assertEquals(emptyList<Pair<Note, Note>>(), actual)
+    Assert.assertEquals(emptyList<Note>(), actual)
   }
 
   @Test
   fun avoid_duplicates_with_same_zap_request() {
     val zapRequest = mockk<Note>()
 
-    val zaps: Map<Note, Note?> = mapOf(
-      zapRequest to mockZapNoteWith("user-1", amount = 100),
-      zapRequest to mockZapNoteWith("user-1", amount = 200),
+    val zaps = mapOf(
+      zapRequest to mockZapNote(zapAmount = 100),
+      zapRequest to mockZapNote(zapAmount = 200),
     )
 
-    val actual = UserZaps.forProfileFeed(zaps)
+    val actual = UserZaps.groupZapsByUser(zaps)
 
     Assert.assertEquals(1, actual.count())
-    Assert.assertEquals(zapRequest, actual.first().first)
     Assert.assertEquals(
       BigDecimal(200),
       (actual.first().second.event as LnZapEventInterface).amount()
     )
   }
 
-  private fun mockZapNoteWith(pubkey: HexKey, amount: Int): Note {
+  @Test
+  fun aggregate_zap_amount_group_by_user() {
+    val zaps = mapOf(
+      mockUserNote("user-1") to mockZapNote(zapAmount = 100),
+      mockUserNote("user-1") to mockZapNote(zapAmount = 200),
+      mockUserNote("user-2") to mockZapNote(zapAmount = 400),
+    )
 
+    val actual = UserZaps.groupZapsByUser(zaps)
+
+    Assert.assertEquals(2, actual.count())
+
+    Assert.assertEquals(
+      BigDecimal(300),
+      (actual[0].second.event as LnZapEventInterface).amount()
+    )
+
+    Assert.assertEquals(
+      BigDecimal(400),
+      (actual[1].second.event as LnZapEventInterface).amount()
+
+    )
+  }
+
+  private fun mockUserNote(authorPubKey: String): Note {
+    val userNote = mockk<Note>()
+    every { userNote.author?.pubkeyHex } returns authorPubKey
+
+    return userNote
+  }
+
+  private fun mockZapNote(zapAmount: Int): Note {
     val lnZapEvent = mockk<LnZapEventInterface>()
-    every { lnZapEvent.amount() } returns amount.toBigDecimal()
-    every { lnZapEvent.pubKey() } returns pubkey
+    every { lnZapEvent.amount() } returns zapAmount.toBigDecimal()
 
     val zapNote = mockk<Note>()
     every { zapNote.event } returns lnZapEvent
